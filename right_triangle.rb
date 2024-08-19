550c53fc703c5d020d66e6ee917356218c203177
RomaVega <roman.alexandrovich.vega@gmail.com>
2024-08-19 21:16:05 +0800

    Громоздко, но для нуба норм. Главное что работает. Работает же?

--- right_triangle.rb
+++ right_triangle.rb
@@ -1,32 +1,56 @@
 print "Please, input triangle's first side length: "
-tr_first_side = gets.chomp.to_f
+tr_first_side = gets.chomp.to_i
 print "Please, input triangle's second side length: "
-tr_second_side = gets.chomp.to_f
+tr_second_side = gets.chomp.to_i
 print "Please, input triangle's third side length: "
-tr_third_side = gets.chomp.to_f
+tr_third_side = gets.chomp.to_i
 
+
+# Проверка равностороннего теугольника 
 if tr_third_side == tr_second_side && tr_second_side == tr_first_side
    puts "Triangle is equilateral"
+   return
 end
 
-if tr_third_side > tr_second_side && tr_third_side > tr_first_side
-hypotenuse = tr_third_side
-  if hypotenuse**2 == tr_second_side**2 + tr_first_side**2
-  puts "Triangle is right"
-    else puts "Triangle is not right"
-  end
-  elsif tr_second_side > tr_third_side && tr_second_side > tr_first_side
-  hypotenuse = tr_second_side
-  if hypotenuse**2 == tr_third_side**2 + tr_first_side**2
-  puts "Triangle is right"
-    else "Triangle is not right"
-  end
-    else
-    hypotenuse = tr_first_side
-      if hypotenuse**2 == tr_second_side**2 + tr_third_side**2
-      puts "Triangle is right"
-      else "Triangle is not right"
-      end
+# Проверка на нулевые вводы
+if tr_third_side == 0 || tr_second_side == 0 || tr_first_side == 0
+   puts "Triangle side cannot be zero"
+   return
 end
+
+# Проверка 3ей стороны, является ли она гипотенузой
+if tr_third_side > tr_second_side && tr_third_side > tr_first_side
+   hypotenuse = tr_third_side
+
+# Вложенная проверка по теореме Пифагора
+   if hypotenuse**2 == tr_second_side**2 + tr_first_side**2
+      puts "Triangle is right"
+   else
+      puts "Triangle is not right"
+   end
+
+# Проверка 2ой стороны, является ли она гипотенузой
+elsif tr_second_side > tr_third_side && tr_second_side > tr_first_side
+  hypotenuse = tr_second_side
+
+# Вложенная проверка по теореме Пифагора  
+    if hypotenuse**2 == tr_third_side**2 + tr_first_side**2
+      puts "Triangle is right"
+    else 
+      puts "Triangle is not right"
+     end
+
+# Проверка 1ой стороны, является ли она гипотенузой 
+elsif tr_first_side > tr_second_side && tr_first_side > tr_third_side
+  hypotenuse = tr_first_side
+
+# Вложенная проверка по теореме Пифагора
+  if hypotenuse**2 == tr_second_side**2 + tr_third_side**2
+    puts "Triangle is right"
+  else 
+    puts "Triangle is not right"
+  end
+end
+
 
  
