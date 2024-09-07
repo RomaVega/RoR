be8b29e515ae93ded1df03cad44c0121d274ad4a
RomaVega <roman.alexandrovich.vega@gmail.com>
2024-09-07 21:32:18 +0800

    Задания к Основы Ruby. Часть 2

--- triangle_area.rb
+++
@@ -1,6 +0,0 @@
-print "Input triangle base, please: "
-tr_base = gets.chomp.to_f
-print "Input triangle height, please: "
-tr_height = gets.chomp.to_f
-tr_area = 0.5*tr_base*tr_height
-puts "Triangle area is: #{tr_area}"
